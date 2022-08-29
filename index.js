43a40b8bf01fe53990bf649fd35d516e7a21579d
hanxl <hanxl.james@gmail.com>
2022-08-29 12:10:57 -0500

    commit tree ae80275ce8b7aed704e6bc374a4e78aca2e18a43

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+
+
+  let giveCustomerFreeSample, chargeTwentyDollars, chargeThirtyDollars, notAllow;
+
+  if (distance <= 400) {
+    giveCustomerFreeSample = true;
+    return "This one is on me!"
+  } else if (distance <= 2000) {
+    chargeTwentyDollars = true;
+    return "That will be twenty bucks."
+  } else if (distance <= 2500) {
+    chargeThirtyDollars = true;
+    return "I will gladly take your thirty bucks."
+  } else if (distance > 2500) {
+    notAllow = true;
+    return "No can do."
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+
+  // if (city == "NYC") {
+  //   return "Ok, sounds good."
+  // } else {
+  //   return "No go."
+  // }
+return city == "NYC" ? "Ok, sounds good." : "No go."
+
+
+
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  
+  switch (tip){
+    case 'generous' :
+      return "Thank you so much."
+      break;
+    case 'not as generous' :
+      return 'Thank you.'
+      break;
+    case 'thanks for everything' :
+      return 'Bye.'
+      break;
+  }
 }
